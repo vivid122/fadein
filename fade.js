4ef52ce590291c1f20790b98e67a1ad06dedc7ab
vivid122 <983807154@qq.com>
2016-03-09 21:17:20 +0800

    Update fade.js

--- fade.js
+++ fade.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 			html+='<div class="items"><h3>'+data[0].year+'</h3></div><div class="items"><h3>'+data[0].tag+'</h3></div>';
 			var i;
 			for(i=1;i<data.length;i++){
-				html+='<div class="items">'+'<span class="date">'+data[i].data+'</span>'+'<h4>'+data[i].name+'</h4></div>';	
+				html+='<div class="items">'+'<span class="date">'+data[i].date+'</span>'+'<h4>'+data[i].name+'</h4></div>';	
 			}
 			$("body").html(html);
 			$(".items").each(function(index){
