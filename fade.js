3afca96f370e11f6746358fa6527e207fa85fa7f
vivid122 <983807154@qq.com>
2016-03-10 08:10:54 +0800

    Update fade.js

--- fade.js
+++ fade.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
 		dataType:"json",
 		success:function(data){
 			var html='';
-			html+='<div class="items"><h3>'+data[0].year+'</h3></div><div class="items"><h3>'+data[0].tag+'</h3></div>';
+			html+='<div class="items"><h3>'+data[0].year+'</h3></div><span class="items"><h3>'+data[0].tag+'</h3></span>';
 			var i;
 			for(i=1;i<data.length;i++){
-				html+='<div class="items">'+'<span class="date">'+data[i].date+'</span>'+'<h4>'+data[i].name+'</h4></div>';	
+				html+='<div class="items">'+'<span class="date">'+data[i].date+'</span>'+'<span>'+data[i].name+'</span></div>';	
 			}
-			$(".line").height(i*50).css("display","none").slideDown(600);
+			$(".line").height(i*80).css("display","none").slideDown(600);
 			$(".contain").html(html);
 			$(".items").each(function(index){
 				$(this).delay(200*index).fadeIn(600);
