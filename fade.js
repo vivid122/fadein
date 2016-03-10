01f493bffc21e04146625d4b0404b60f05972af4
vivid122 <983807154@qq.com>
2016-03-10 08:58:10 +0800

    Update fade.js

--- fade.js
+++ fade.js
@@ -6,16 +6,16 @@ $(document).ready(function(){
 		dataType:"json",
 		success:function(data){
 			var html='';
-			html+='<div class="items"><h3>'+data[0].year+'</h3><span class="items"><b>'+data[0].tag+'</b></span></div>';
+			html+='<div class="items"><b>'+data[0].year+'</b><span><b>'+data[0].tag+'</b></span></div>';
 			var i;
 			for(i=1;i<data.length;i++){
-				html+='<div class="items">'+'<span class="date">'+data[i].date+'</span>'+'<b>'+'   '+data[i].name+'</b></div>';	
+				html+='<div class="items">'+'<span class="date">'+data[i].date+'</span>'+'<b>'+' '+data[i].name+'</b></div>';	
 			}
 			$(".line").height(i*60).css("display","none").slideDown(600);
 			$(".contain").html(html);
-			$(".items").each(function(index){
+			$(".items").addClass("on").each(function(index){
 				$(this).delay(200*index).fadeIn(600);
-			}).addClass("on");
+			});
 		}
 	});
 });
